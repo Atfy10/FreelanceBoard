09680c4fee329967c4346fa48d0d0844788d2f65
John Nagi <johnnalarja@gmail.com>
2025-08-08 21:55:56 +0300

    updated UI for job listings -- added CRUD operations

---
+++ FreelanceBoard.Web/FreelanceBoard.MVC/Models/CreateJobViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FreelanceBoard.MVC.Models
+{
+    public class CreateJobViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public List<string> SkillNames { get; set; }
+    }
+
+}
