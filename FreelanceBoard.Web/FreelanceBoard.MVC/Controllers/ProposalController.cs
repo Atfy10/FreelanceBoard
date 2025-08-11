a8b68cf9ea153798f2d2ffc727d6b2e4a3cc8e25
John Nagi <johnnalarja@gmail.com>
2025-08-11 16:00:42 +0300

    Connected the get all proposals by job id to the MVC

---
+++ FreelanceBoard.Web/FreelanceBoard.MVC/Controllers/ProposalController.cs
@@ -0,0 +1,34 @@
+﻿using FreelanceBoard.Core.Domain.Entities;
+using FreelanceBoard.MVC.Extensions;
+using FreelanceBoard.MVC.Models;
+using FreelanceBoard.MVC.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreelanceBoard.MVC.Controllers
+{
+    public class ProposalController : Controller
+    {
+        private readonly IProposalService _proposalService;
+        private readonly OperationExecutor _executor;
+
+        public ProposalController(IProposalService proposalService, OperationExecutor executor)
+        {
+            _proposalService = proposalService;
+            _executor = executor;
+        }
+        public async Task<IActionResult> JobProposal(int id)
+        {
+            List<ProposalViewModel> job = null;
+            var success = await _executor.Execute(
+                async () =>
+                { job = await _proposalService.GetProposalsByJobIdAsync(id, HttpContext); },
+                error => ModelState.AddModelError(string.Empty, error)
+            );
+
+            if (!success || job == null)
+                return View("NotFound");
+
+            return View("JobProposal",job);
+        }
+    }
+}
--- FreelanceBoard.Web/FreelanceBoard.MVC/Services/Implementations/ProposalService.cs
+++ FreelanceBoard.Web/FreelanceBoard.MVC/Services/Implementations/ProposalService.cs
@@ -1,39 +1,37 @@
-﻿using FreelanceBoard.MVC.Extensions;
+﻿using FreelanceBoard.Core.Queries.Interfaces;
+using FreelanceBoard.MVC.Extensions;
 using FreelanceBoard.MVC.Models;
 using FreelanceBoard.MVC.Services.Interfaces;
 using System.Net.Http.Headers;
 
 namespace FreelanceBoard.MVC.Services.Implementations
 {
-    public class JobService : IJobService
+    public class ProposalService : IProposalService
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public JobService(IHttpClientFactory httpClientFactory)
+        public ProposalService(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
-        //get all jobs using the user id
-        public async Task<List<ClientDashboardViewModel>> GetAllJobsAsync(HttpContext httpContext)
+        public async Task<List<ProposalViewModel>> GetProposalsByJobIdAsync(int id, HttpContext httpContext)
         {
             var token = httpContext.User.GetAccessToken();
             var client = _httpClientFactory.CreateClient("FreelanceApiClient");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var userId = httpContext.User?.GetUserId();
-            var response = await client.GetAsync($"/api/jobs/user/{userId}/jobs");
+
+            var response = await client.GetAsync($"/api/Proposal/job/{id}/proposals");
 
             if (!response.IsSuccessStatusCode)
-                throw new ApplicationException("Failed to fetch jobs");
+                throw new ApplicationException("Failed to fetch proposals");
 
-            var apiResult = await response.Content.ReadFromJsonAsync<ApiErrorResponse<List<ClientDashboardViewModel>>>();
+            var apiResult = await response.Content.ReadFromJsonAsync<ApiErrorResponse<List<ProposalViewModel>>>();
 
             if (apiResult is null || !apiResult.IsSuccess)
-                throw new ApplicationException(apiResult?.Message ?? "No jobs found");
+                throw new ApplicationException(apiResult?.Message ?? "No proposals found");
 
             return apiResult.Data;
         }
     }
-
-
 }
