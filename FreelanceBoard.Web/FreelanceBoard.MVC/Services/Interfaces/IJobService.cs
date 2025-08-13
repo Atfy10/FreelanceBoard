2c70d3d60bcfc997c2fc93a1192bd893d145112c
Abdulrahman Al-Atfy <abdo.mo.atfy789@gmail.com>
2025-08-13 20:08:47 +0300

    simple modfication

--- FreelanceBoard.Web/FreelanceBoard.MVC/Services/Interfaces/IJobService.cs
+++ FreelanceBoard.Web/FreelanceBoard.MVC/Services/Interfaces/IJobService.cs
@@ -4,9 +4,9 @@ namespace FreelanceBoard.MVC.Services.Interfaces
 {
     public interface IJobService
     {
-		Task CreateJobAsync(JobCreateViewModel model, HttpContext httpContext);
-		Task<List<ClientDashboardViewModel>> GetAllJobsAsync(HttpContext httpContext, int page = 1);
-        Task<List<JobViewModel>> GetAllJobsSortedAsync(HttpContext httpContext, string sortBy, int page = 1);
+        Task CreateJobAsync(JobCreateViewModel model, HttpContext httpContext);
+        Task<List<ClientDashboardViewModel>> GetAllJobsAsync(HttpContext httpContext, int page = 1);
+        Task<List<JobViewModel>> GetAllJobsSortedAsync(HttpContext httpContext, string sortBy, bool isAscending = true, int page = 1);
         Task<JobViewModel> GetJobByIdAsync(HttpContext httpContext, int id);
 
     }
